086c21013a30cbea3c1660e0eb127f4bb81f0141
refactoror <deadletter@aol.com>
2013-10-06 19:17:27 +0000

    add try-block handler

---
+++ sel-blocks-fx_xpi/chrome/content/extensions/selenium-executionloop-handleAsTryBlock.js
@@ -0,0 +1,23 @@
+// selbocks name-space
+(function($$){
+  /* This function replaces native Selenium command handling while inside a try block.
+   */
+  $$.handleAsTryBlock = function()
+  {
+    try {
+      selenium.browserbot.runScheduledPollers();
+      this._executeCurrentCommand();
+      this.continueTestWhenConditionIsTrue();
+    } catch (e) {
+      var isHandled = this._handleCommandError(e); // causes command to be marked in red
+      var handlerAttrs = $$.getInterceptAttrs();
+      var isCaught = handlerAttrs.handleError(e);
+      if (isCaught) {
+        this.continueTest();
+      } else {
+        this.testComplete();
+      }
+    }
+  };
+
+}(selblocks));
