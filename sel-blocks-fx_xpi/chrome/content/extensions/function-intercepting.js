d5e1155060d0b3f2151874a3f1e1b3cdd1c948c1
refactoror <deadletter@aol.com>
2014-02-25 21:32:27 -0500

    Issue#2: Fix for cases where function or label are the first command

--- sel-blocks-fx_xpi/chrome/content/extensions/function-intercepting.js
+++ sel-blocks-fx_xpi/chrome/content/extensions/function-intercepting.js
@@ -7,15 +7,15 @@
 
   // execute the given function before each call of the specified function
   $$.fn.interceptBefore = function(targetObj, targetFnName, _fn) {
-    var existing_fn = targetObj[targetFnName] = _fn;
+    var existing_fn = targetObj[targetFnName];
     targetObj[targetFnName] = function() {
       _fn.call(this);
-      existing_fn.call(this);
+      return existing_fn.call(this);
     };
   };
   // execute the given function after each call of the specified function
   $$.fn.interceptAfter = function(targetObj, targetFnName, _fn) {
-    var existing_fn = targetObj[targetFnName] = _fn;
+    var existing_fn = targetObj[targetFnName];
     targetObj[targetFnName] = function() {
       existing_fn.call(this);
       _fn.call(this);
@@ -33,7 +33,7 @@
 
   // replace the specified function, saving the original function on a stack
   $$.fn.interceptPush = function(targetObj, targetFnName, _fn, frameAttrs) {
-// $$.LOG.warn("interceptPush " + (frameAttrs ? frameAttrs : ""));
+    // $$.LOG.warn("interceptPush " + (frameAttrs ? frameAttrs : ""));
     var frame = {
        targetObj: targetObj
       ,targetFnName: targetFnName
@@ -46,7 +46,7 @@
   // restore the most recent function replacement
   $$.fn.interceptPop = function() {
     var frame = $$.fn.interceptStack.pop();
-// $$.LOG.warn("interceptPop " + (frame.attrs ? frame.attrs : ""));
+    // $$.LOG.warn("interceptPop " + (frame.attrs ? frame.attrs : ""));
     frame.targetObj[frame.targetFnName] = frame.savedFn;
   };
   $$.fn.getInterceptAttrs = function() {
