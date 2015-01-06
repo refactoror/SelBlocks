33099c5855d0f752c38473c4020e1a40beee6e29
refactoror <deadletter@aol.com>
2015-01-05 20:23:23 -0500

    split out fileReader.js

--- sel-blocks-fx_xpi/chrome/content/extensions/selblocks.js
+++ sel-blocks-fx_xpi/chrome/content/extensions/selblocks.js
@@ -1111,13 +1111,13 @@ function $X(xpath, contextNode, resultType) {
   Selenium.prototype.doLoadJsonVars = function(filepath, selector)
   {
     assert(filepath, " Requires a JSON file path or URL.");
-    var jsonReader = new JSONReader(filepath);
+    var jsonReader = new $$.JSONReader(filepath);
     loadVars(jsonReader, "JSON object", filepath, selector);
   };
   Selenium.prototype.doLoadXmlVars = function(filepath, selector)
   {
     assert(filepath, " Requires an XML file path or URL.");
-    var xmlReader = new XmlReader(filepath);
+    var xmlReader = new $$.XmlReader(filepath);
     loadVars(xmlReader, "XML element", filepath, selector);
   };
   Selenium.prototype.doLoadVars = function(filepath, selector)
@@ -1164,7 +1164,7 @@ function $X(xpath, contextNode, resultType) {
     enterLoop(
       function(loop) {  // validate
           assert(jsonpath, " Requires a JSON file path or URL.");
-          loop.jsonReader = new JSONReader();
+          loop.jsonReader = new $$.JSONReader();
           var localVarNames = loop.jsonReader.load(jsonpath);
           return localVarNames;
       }
@@ -1186,7 +1186,7 @@ function $X(xpath, contextNode, resultType) {
     enterLoop(
       function(loop) {  // validate
           assert(xmlpath, " 'forXml' requires an XML file path or URL.");
-          loop.xmlReader = new XmlReader();
+          loop.xmlReader = new $$.XmlReader();
           var localVarNames = loop.xmlReader.load(xmlpath);
           return localVarNames;
       }
@@ -1518,292 +1518,6 @@ function $X(xpath, contextNode, resultType) {
       this.hasNext = function() { return (cur < ary.length); };
       this.next = function() { if (this.hasNext()) { return ary[cur++]; } };
     }(arrayObject);
-  };
-
-  // ==================== Data Files ====================
-  // Adapted from the datadriven plugin
-  // http://web.archive.org/web/20120928080130/http://wiki.openqa.org/display/SEL/datadriven
-
-  function XmlReader()
-  {
-    var varsets = null;
-    var varNames = null;
-    var curVars = null;
-    var varsetIdx = 0;
-
-    // load XML file and return the list of var names found in the first <VARS> element
-    this.load = function(filepath)
-    {
-      var fileReader = new FileReader();
-      var fileUrl;
-      // in order to not break existing tests the IDE will still use urlFor,
-      // on the server it just breaks things. Data can be anywhere on the net,
-      // accessible through proper CORS headers.
-      if (globalContext.onServer === true) {
-        fileUrl = filepath;
-      } else {
-        fileUrl = urlFor(filepath);
-      }
-      var xmlHttpReq = fileReader.getDocumentSynchronous(fileUrl);
-      $$.LOG.info("Reading from: " + fileUrl);
-
-      var fileObj = xmlHttpReq.responseXML; // XML DOM
-      varsets = fileObj.getElementsByTagName("vars"); // HTMLCollection
-      if (varsets === null || varsets.length === 0) {
-        throw new Error("A <vars> element could not be loaded, or <testdata> was empty.");
-      }
-
-      curVars = 0;
-      varNames = attrNamesFor(varsets[0]);
-      return varNames;
-    };
-
-    this.EOF = function() {
-      return (curVars === null || curVars >= varsets.length);
-    };
-
-    this.next = function()
-    {
-      if (this.EOF()) {
-        $$.LOG.error("No more <vars> elements to read after element #" + varsetIdx);
-        return;
-      }
-      varsetIdx++;
-      $$.LOG.debug(varsetIdx + ") " + serializeXml(varsets[curVars]));  // log each name & value
-
-      var expected = countAttrs(varsets[0]);
-      var found = countAttrs(varsets[curVars]);
-      if (found !== expected) {
-        throw new Error("Inconsistent <testdata> at <vars> element #" + varsetIdx
-          + "; expected " + expected + " attributes, but found " + found + "."
-          + " Each <vars> element must have the same set of attributes."
-        );
-      }
-      setupStoredVars(varsets[curVars]);
-      curVars++;
-    };
-
-    //- retrieve the names of each attribute on the given XML node
-    function attrNamesFor(node) {
-      var attrNames = [];
-      var varAttrs = node.attributes; // NamedNodeMap
-      var v;
-      for (v = 0; v < varAttrs.length; v++) {
-        attrNames.push(varAttrs[v].nodeName);
-      }
-      return attrNames;
-    }
-
-    //- determine how many attributes are present on the given node
-    function countAttrs(node) {
-      return node.attributes.length;
-    }
-
-    //- set selenium variables from given XML attributes
-    function setupStoredVars(node) {
-      var varAttrs = node.attributes; // NamedNodeMap
-      var v;
-      for (v = 0; v < varAttrs.length; v++) {
-        var attr = varAttrs[v];
-        if (null === varsets[0].getAttribute(attr.nodeName)) {
-          throw new Error("Inconsistent <testdata> at <vars> element #" + varsetIdx
-            + "; found attribute " + attr.nodeName + ", which does not appear in the first <vars> element."
-            + " Each <vars> element must have the same set of attributes."
-          );
-        }
-        storedVars[attr.nodeName] = attr.nodeValue;
-      }
-    }
-
-    //- format the given XML node for display
-    function serializeXml(node) {
-      if (XMLSerializer !== "undefined") {
-        return (new XMLSerializer()).serializeToString(node) ;
-      }
-      if (node.xml) { return node.xml; }
-      throw "XMLSerializer is not supported or can't serialize " + node;
-    }
-  }
-
-
-  function JSONReader()
-  {
-    var varsets = null;
-    var varNames = null;
-    var curVars = null;
-    var varsetIdx = 0;
-
-    // load JSON file and return the list of var names found in the first object
-    this.load = function(filepath)
-    {
-      var fileReader = new FileReader();
-      var fileUrl;
-      // in order to not break existing tests the IDE will still use urlFor,
-      // on the server it just breaks things. Data can be anywhere on the net,
-      // accessible through proper CORS headers.
-      if (globalContext.onServer === true) {
-        fileUrl = filepath;
-      } else {
-        fileUrl = urlFor(filepath);
-      }
-      var xmlHttpReq = fileReader.getDocumentSynchronous(fileUrl);
-      $$.LOG.info("Reading from: " + fileUrl);
-
-      var fileObj = xmlHttpReq.responseText;
-      fileObj = fileObj.replace("/\uFFFD/g", "").replace(/\0/g, "");
-      $$.LOG.info(fileObj);
-      varsets = eval(fileObj);
-      if (varsets === null || varsets.length === 0) {
-        throw new Error("A JSON object could not be loaded, or the file was empty.");
-      }
-
-      curVars = 0;
-      varNames = attrNamesFor(varsets[0]);
-      return varNames;
-    };
-
-    this.EOF = function() {
-      return (curVars === null || curVars >= varsets.length);
-    };
-
-    this.next = function()
-    {
-      if (this.EOF()) {
-        $$.LOG.error("No more JSON objects to read after object #" + varsetIdx);
-        return;
-      }
-      varsetIdx++;
-      $$.LOG.debug(varsetIdx + ") " + serializeJson(varsets[curVars]));  // log each name & value
-
-      var expected = countAttrs(varsets[0]);
-      var found = countAttrs(varsets[curVars]);
-      if (found !== expected) {
-        throw new Error("Inconsistent JSON object #" + varsetIdx
-          + "; expected " + expected + " attributes, but found " + found + "."
-          + " Each JSON object must have the same set of attributes."
-        );
-      }
-      setupStoredVars(varsets[curVars]);
-      curVars++;
-    };
-
-    //- retrieve the names of each attribute on the given object
-    function attrNamesFor(obj) {
-      var attrNames = [];
-      var attrName;
-      for (attrName in obj) {
-        attrNames.push(attrName);
-      }
-      return attrNames;
-    }
-
-    //- determine how many attributes are present on the given obj
-    function countAttrs(obj) {
-      var n = 0;
-      var attrName;
-      for (attrName in obj) {
-        n++;
-      }
-      return n;
-    }
-
-    //- set selenium variables from given JSON attributes
-    function setupStoredVars(obj) {
-      var attrName;
-      for (attrName in obj) {
-        if (null === varsets[0][attrName]) {
-          throw new Error("Inconsistent JSON at object #" + varsetIdx
-            + "; found attribute " + attrName + ", which does not appear in the first JSON object."
-            + " Each JSON object must have the same set of attributes."
-          );
-        }
-        storedVars[attrName] = obj[attrName];
-      }
-    }
-
-    //- format the given JSON object for display
-    function serializeJson(obj) {
-      var json = uneval(obj);
-      return json.substring(1, json.length-1);
-    }
-  }
-
-  function urlFor(filepath) {
-    if (filepath.indexOf("http") == 0) {
-      return filepath;
-    }
-    var URL_PFX = "file://";
-    var url = filepath;
-    if (filepath.substring(0, URL_PFX.length).toLowerCase() !== URL_PFX) {
-      testCasePath = testCase.file.path.replace("\\", "/", "g");
-      var i = testCasePath.lastIndexOf("/");
-      url = URL_PFX + testCasePath.substr(0, i) + "/" + filepath;
-    }
-    return url;
-  }
-
-
-  // ==================== File Reader ====================
-  // Adapted from the include4ide plugin
-
-  function FileReader() {}
-
-  FileReader.prototype.prepareUrl = function(url) {
-    var absUrl;
-    // htmlSuite mode of SRC? TODO is there a better way to decide whether in SRC mode?
-    if (window.location.href.indexOf("selenium-server") >= 0) {
-      $$.LOG.debug("FileReader() is running in SRC mode");
-      // there's no need to absolutify the url, the browser will do that for you
-      // when you make the request. The data may reside anywhere on the site, or
-      // within the "virtual directory" created by the selenium server proxy.
-      // I don't want to limit the ability to parse files that actually exist on
-      // the site, like sitemaps or JSON responses to api calls.
-      absUrl = url;
-    }
-    else {
-      absUrl = absolutify(url, selenium.browserbot.baseUrl);
-    }
-    $$.LOG.debug("FileReader() using URL to get file '" + absUrl + "'");
-    return absUrl;
-  };
-
-  FileReader.prototype.getDocumentSynchronous = function(url) {
-    var absUrl = this.prepareUrl(url);
-    var requester = this.newXMLHttpRequest();
-    if (!requester) {
-      throw new Error("XMLHttp requester object not initialized");
-    }
-    requester.open("GET", absUrl, false); // synchronous (we don't want selenium to go ahead)
-    try {
-      requester.send(null);
-    }
-    catch(e) {
-      throw new Error("Error while fetching URL '" + absUrl + "':: " + e);
-    }
-    if (requester.status !== 200 && requester.status !== 0) {
-      throw new Error("Error while fetching " + absUrl
-        + " server response has status = " + requester.status + ", " + requester.statusText );
-    }
-    return requester;
-  };
-
-  FileReader.prototype.newXMLHttpRequest = function() {
-    var requester = 0;
-    try {
-      // for IE/ActiveX
-      if (window.ActiveXObject) {
-        try {       requester = new ActiveXObject("Msxml2.XMLHTTP"); }
-        catch(ee) { requester = new ActiveXObject("Microsoft.XMLHTTP"); }
-      }
-      // Native XMLHttp
-      else if (window.XMLHttpRequest) {
-        requester = new XMLHttpRequest();
-      }
-    }
-    catch(e) {
-      throw new Error("Your browser has to support XMLHttpRequest in order to read data files\n" + e);
-    }
-    return requester;
   };
 
 }(selblocks));
